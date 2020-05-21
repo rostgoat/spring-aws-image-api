5e6a2481f0126fc1d6bbeafafdab153dfacdd63a
Rost Myshkin <rostmyshkin@gmail.com>
2020-05-21 14:10:37 -0700

    methods in controller to upload data into s3

--- src/main/java/com/rm/awsimageupload/profile/UserProfileService.java
+++ src/main/java/com/rm/awsimageupload/profile/UserProfileService.java
@@ -1,9 +1,11 @@
 package com.rm.awsimageupload.profile;
 
 import java.util.List;
+import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 @Service
 public class UserProfileService {
@@ -18,4 +20,12 @@ public class UserProfileService {
     List<UserProfile> getUserProfiles() {
         return userProfileDataAccessService.getUserProfiles();
     }
+
+	void uploadUserProfileImage(UUID userProfileId, MultipartFile file) {
+        // 1. check if image is not empty
+        // 2. if file is an image
+        // 3. the user exists in db
+        // 4. grab some meta from file if any
+        // 5. store image in s3 and update db with s3 image url
+	}
 }
